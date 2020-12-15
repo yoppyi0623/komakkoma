063def0b26004b45dc3266bf52620a15fdb05beb
yoppyi0623 <59519214+yoppyi0623@users.noreply.github.com>
2020-12-15 21:20:24 +0900

    implement re

--- discordbot.py
+++ discordbot.py
@@ -16,24 +16,4 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.command()
-async def uso(ctx):
-    await ctx.send('なんだろう嘘つくのやめてもらっていいですか')
-    
-@bot.command()
-async def takuma(ctx):
-    await ctx.send(':takumakaranopurezento:')
-
-@bot.command()
-async def urnlo(ctx):
-    await ctx.send('子供がいるとは思えないほどの華奢で処女の欲求不満な敏感爆乳魅力ママのうるんるちゃんの愛情たっぷり朝一絞りたての至極の母乳小便をお口の中にぴゅっぴゅされながら義務教育を終えたい')   
-
-@bot.command()
-async def kakikama(ctx):
-    await ctx.send('手コキフェラパイずりしながらイっちゃえ:heart:イっちゃえ:heart:って言ってくるドSで押しに弱いチ●ポ大好きっ娘ご奉仕生意気■リ乳ムチムチ色白正統派黒髪清楚美少女かきかまちゃんとイチャラブ本気▲ませ交尾コンドーム突き破り中出し乳ブルンブルンベロチュー体液汁だくだくべちゃ濡れセックスしたい')
-
-@bot.command()
-    async def start(ctx):
-        await ctx.send('ONLINE')
-        
 bot.run(token)
