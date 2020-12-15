ed9a92fa061049c1a233296cbcc679c07a58edea
yoppyi0623 <59519214+yoppyi0623@users.noreply.github.com>
2020-12-15 17:59:28 +0900

    implement uso

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,9 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def uso(ctx):
+    await ctx.send('なんだろう嘘つくのやめてもらっていいですか')
+
 
 bot.run(token)
